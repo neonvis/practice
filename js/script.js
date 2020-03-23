47388a5c429b41abbcb7074497ca5c13078b7eea
Vital <neonvia@gmail.com>
2020-03-23 12:04:58 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// menu.onclick = function myFunction() {
+// 	var x = document.getElementById("myTopnav");
+
+// 	if(x.className === "topnav") {
+// 		x.className += "responsive";
+// 	} else{
+// 		x.className = "topnav";
+// 	}
+// }
+
+// window.onload = function(){
+// 	let x = document.querySelector("#myTopnav");
+// 	document.querySelector("#menu").onclick = function(){
+// 		x.classList.toggle('responsive');
+// 	}
+// }
+
+console.log(LeftBorderWidth);
+var LeftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
